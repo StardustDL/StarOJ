a0a0ab75c68a319819776337d639ad1c8510460c
StardustDL <stardustdl@163.com>
2020-07-12 15:58:45 +0800

    fix id4 public origin

--- src/StarOJ/Server/Startup.cs
+++ src/StarOJ/Server/Startup.cs
@@ -83,10 +83,10 @@ namespace StarOJ.Server
             {
                 app.UseExceptionHandler("/Error");
                 // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-                // app.UseHsts();
+                app.UseHsts();
             }
 
-            // app.UseHttpsRedirection();
+            app.UseHttpsRedirection();
             app.UseBlazorFrameworkFiles();
             app.UseStaticFiles();
 
